f6ae78b84ace987d6c0f8dcdfa1c4298d2d731cb
HyejinKim624 <un123k@naver.com>
2023-11-16 16:18:23 +0900

    second commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,6 +14,7 @@ import com.example.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     // test1에서 수정
     // test1에서 수정2
+    // test1에서 수정3
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
